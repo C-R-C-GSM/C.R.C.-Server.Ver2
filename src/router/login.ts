13361e3aefab5013138a3bcfc168b490872e981f
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-05-26 10:28:20 +0900

    login setting
    login setting

---
+++ src/router/login.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const login = express.Router();
+const mysql = require("mysql")
+
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER ,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
+});
+login.get("/", (request: Request, response: Response, next: NextFunction) => {
+    console.log('get success');
+});
+
+connection.
+
+export = login;
