3457c9ec71fdef51bd654d95581cb8825ef9f5e1
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-06-15 21:02:37 +0900

    code refactoring
    code refactoring

--- src/router/suggest.ts
+++ src/router/suggest.ts
@@ -48,43 +48,35 @@ suggest.post('/suggest_register',(req:Request,res:Response,next:NextFunction) =>
 });
 
 suggest.post('/empathy',(req:Request,res:Response,next:NextFunction) => {
-    if(req.body.empathy) {
-        connection.query("SELECT empathy FROM crcdb.suggest WHERE suggestid = ?",[req.body.suggestid],
-        function(err:Error,results:any,fields:any) {
-            if(err) {
-                res.json({success:false,code:-100,message:'cannot connect db'});
-                console.log(err)
-            } else {
-                connection.query("UPDATE crcdb.suggest SET empathy = ? WHERE suggestid = ?",[results+1,req.body.suggestid],
-                function(err1:Error,results1:any,fields1:any) {
-                    if(err) {
-                        res.json({success:false,code:-100,message:'cannot connect db'});
-                        console.log(err)
-                    } else {
-                        res.json({success:true,code:0,message:'empathy success'})
-                    }
-                })
-            }
-        })
-    } else {
-        res.json({success:false,code:-1,message:'empathy가 존재하지 않습니다.'})
-    }
+    connection.query("SELECT empathy FROM crcdb.suggest WHERE suggestid = ?",[req.body.suggestid],
+    function(err:Error,results:any,fields:any) {
+        if(err) {
+            res.json({success:false,code:-100,message:'cannot connect db'});
+            console.log(err)
+        } else {
+            connection.query("UPDATE crcdb.suggest SET empathy = ? WHERE suggestid = ?",[results+1,req.body.suggestid],
+            function(err1:Error,results1:any,fields1:any) {
+                if(err) {
+                    res.json({success:false,code:-100,message:'cannot connect db'});
+                    console.log(err)
+                } else {
+                    res.json({success:true,code:0,message:'empathy success'})
+                }
+            })
+        }
+    })
 });
 
 suggest.post('/reply',(req:Request,res:Response,next:NextFunction) => {
-    if(req.body.reply) {
-        connection.query("UPDATE crcdb.suggest SET reply = ? WHERE suggestid = ?",[req.body.reply,req.body.suggestid],
-        function(err:Error,results:any,fields:any) {
-            if(err) {
-                res.json({success:false,code:-100,message:'cannot connect db'});
-                console.log(err)
-            } else {
-                res.json({success:true,code:0,message:'reply success'});
-            }
-        })
-    } else {
-        res.json({success:false,code:-1,message:'reply가 존재하지 않습니다.'});
-    }
+    connection.query("UPDATE crcdb.suggest SET reply = ? WHERE suggestid = ?",[req.body.reply,req.body.suggestid],
+    function(err:Error,results:any,fields:any) {
+        if(err) {
+            res.json({success:false,code:-100,message:'cannot connect db'});
+            console.log(err)
+        } else {
+            res.json({success:true,code:0,message:'reply success'});
+        }
+    });
 });
 
 export = suggest;
