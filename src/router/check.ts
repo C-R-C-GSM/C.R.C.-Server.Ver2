34fa0cbc239d7a4d17ad9463229fb145ed6812e2
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-06-21 18:00:25 +0900

    add /com_student
    들어오는 지문 인식 코드를 받아온 다음 값에 따라 다른 db값 체크

--- src/router/check.ts
+++ src/router/check.ts
@@ -22,17 +22,46 @@ var connection = mysql.createConnection({
 connection.connect();
 
 let total_num:number = 0;
-
+let student_id:string;
 check.post('/come_student', (req:Request, res:Response) => {
     total_num +=1;
+    student_id = req.body.student_id;
+
+    if(student_id.substring(0,1) == "1") {
+        connection.query("UPDATE crcdb.1student SET check1 = ? WHERE certify1 = ?",[1,student_id],
+        function(err:Error,results:any, fields:any) {
+            if(err) console.log(err)
+            else {
+                console.log('success to change')
+            }
+        });
+    } else if(student_id.substring(0,1) == "2") {
+        connection.query("UPDATE crcdb.2student SET check2 = ? WHERE certify2 = ?",[1,student_id],
+        function(err:Error,results:any, fields:any) {
+            if(err) console.log(err)
+            else {
+                console.log('success to change')
+            }
+        });
+    } else {
+        connection.query("UPDATE crcdb.3student SET check3 = ? WHERE certify3 = ?",[1,student_id],
+        function(err:Error,results:any, fields:any) {
+            if(err) console.log(err)
+            else {
+                console.log('success to change')
+            }
+        });
+    }
 });
+
+
   
 
 check.post('/total', (req:Request, res:Response) => {
     res.json({success:true, code:0,message:'total_data_send',total_num:total_num})
 });
 
-check.post('/one', (req:Request, res:Response) => {
+check.get('/one', (req:Request, res:Response) => {
   connection.query("SELECT * FROM crcdb.1student",
   function(err:Error,results:any, fields:any) {
       if(err) res.json({success:false,code:-100,message:'cannot connect db'});
@@ -42,7 +71,7 @@ check.post('/one', (req:Request, res:Response) => {
   });
 });
 
-check.post('/two', (req:Request, res:Response) => {
+check.get('/two', (req:Request, res:Response) => {
     connection.query("SELECT * FROM crcdb.2student",
     function(err:Error,results:any, fields:any) {
         if(err) res.json({success:false,code:-100,message:'cannot connect db'});
@@ -52,7 +81,7 @@ check.post('/two', (req:Request, res:Response) => {
     });
 });
 
-check.post('/three', (req:Request, res:Response) => {
+check.get('/three', (req:Request, res:Response) => {
     connection.query("SELECT * FROM crcdb.3student",
     function(err:Error,results:any, fields:any) {
         if(err) res.json({success:false,code:-100,message:'cannot connect db'});
