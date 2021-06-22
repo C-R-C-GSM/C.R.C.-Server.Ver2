6282624c6ba7cfd87378ed8dc22494cc44608882
ingyocode <69563418+ingyocode@users.noreply.github.com>
2021-06-22 18:16:03 +0900

    delete unused code
    delete unused code

--- src/router/check.ts
+++ src/router/check.ts
@@ -22,9 +22,41 @@ var connection = mysql.createConnection({
 connection.connect();
 
 let total_num:number = 0;
+let student_id:string;
+let oneData:JSON;
+let twoData:JSON;
+let threeData:JSON;
 
 check.post('/come_student', (req:Request, res:Response) => {
     total_num +=1;
+    student_id = req.body.student_num;
+    console.log(student_id);
+    res.json({Done:"Done"});
+    if(student_id.substring(0,1) == "1") {
+        connection.query("UPDATE crcdb.1student SET check1 = ? WHERE certify1 = ?",[1,student_id],
+        function(err:Error,results:any, fields:any) {
+            if(err) console.log(err)
+            else {
+                console.log('success to change')
+            }
+        });
+    } else if(student_id.substring(0,1) == "2") {
+        connection.query("UPDATE crcdb.2student SET check2 = ? WHERE certify2 = ?",[1,student_id],
+        function(err:Error,results:any, fields:any) {
+            if(err) console.log(err)
+            else {
+                console.log('success to change')
+            }
+        });
+    } else {
+        connection.query("UPDATE crcdb.3student SET check3 = ? WHERE certify3 = ?",[1,student_id],
+        function(err:Error,results:any, fields:any) {
+            if(err) console.log(err)
+            else {
+                console.log('success to change')
+            }
+        });
+    }
 });
   
 
@@ -32,32 +64,32 @@ check.post('/total', (req:Request, res:Response) => {
     res.json({success:true, code:0,message:'total_data_send',total_num:total_num})
 });
 
-check.post('/one', (req:Request, res:Response) => {
-  connection.query("SELECT * FROM crcdb.1student",
+check.get('/one', (req:Request, res:Response) => {
+  connection.query("SELECT student_data1,student_name1,check FROM crcdb.1student",
   function(err:Error,results:any, fields:any) {
       if(err) res.json({success:false,code:-100,message:'cannot connect db'});
       else {
-          res.json({success:true, code:0, message:'check success'})
+          res.json({success:true, code:0, message:'check success',oneData:results})
       }
   });
 });
 
-check.post('/two', (req:Request, res:Response) => {
-    connection.query("SELECT * FROM crcdb.2student",
+check.get('/two', (req:Request, res:Response) => {
+    connection.query("SELECT student_data2,student_name2,check FROM crcdb.2student",
     function(err:Error,results:any, fields:any) {
         if(err) res.json({success:false,code:-100,message:'cannot connect db'});
         else {
-            res.json({success:true, code:0, message:'check success'})
+            res.json({success:true, code:0, message:'check success',twoData:results})
         }
     });
 });
 
-check.post('/three', (req:Request, res:Response) => {
-    connection.query("SELECT * FROM crcdb.3student",
+check.get('/three', (req:Request, res:Response) => {
+    connection.query("SELECT student_data3,student_name3,check FROM crcdb.3student",
     function(err:Error,results:any, fields:any) {
         if(err) res.json({success:false,code:-100,message:'cannot connect db'});
         else {
-            res.json({success:true, code:0, message:'check success'})
+            res.json({success:true, code:0, message:'check success',threeData:results})
         }
     });
 });
